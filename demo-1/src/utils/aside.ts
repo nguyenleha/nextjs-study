c2b855064c0410bdfb625b21396f7f9aa76b6e89
Nguyễn Lê Hà <65054260+nguyenleha@users.noreply.github.com>
2025-09-30 11:22:19 +0700

    Update aside.ts

--- demo-1/src/utils/aside.ts
+++ demo-1/src/utils/aside.ts
@@ -16,44 +16,6 @@ export const useConfigAside = (): AsideType[] => {
             ],
         },
         {
-            id: 4,
-            name: 'マスタ',
-            aside_btn: true,
-            path: '',
-            sub: [
-                {
-                    name: '排出事業場',
-                    path: '/admin',
-                    new_tab: false,
-                },
-                {
-                    name: '作業内容',
-                    path: '/admin',
-                    new_tab: false,
-                },
-                {
-                    name: '配車',
-                    path: '/admin',
-                    new_tab: false,
-                },
-                {
-                    name: '作業員',
-                    path: '/admin',
-                    new_tab: false,
-                },
-                {
-                    name: '車両',
-                    path: '/admin',
-                    new_tab: false,
-                },
-                {
-                    name: '配車表',
-                    path: '/admin',
-                    new_tab: false,
-                },
-            ],
-        },
-        {
             id: 5,
             name: 'ユーザ管理',
             aside_btn: true,
